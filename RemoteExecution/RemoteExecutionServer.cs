b9003fc6723075dfc28fb9c3861024cd1c763e2f
Mike Rousos <MikeRou@Microsoft.com>
2016-11-30 11:12:51 -0800

    Add initial project scaffolding

---
+++ RemoteExecution/RemoteExecutionServer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RemoteExecution
+{
+    public class RemoteExecutionServer : IDisposable
+    {
+        public RemoteExecutionServer()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
