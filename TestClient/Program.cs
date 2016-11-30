3c590298b633721da8f625651f49fa9b53245509
Mike Rousos <MikeRou@Microsoft.com>
2016-11-30 12:11:56 -0800

    Add simple test client

---
+++ TestClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Text;
+
+using RemoteExecution;
+
+namespace TestClient
+{
+    public class Program
+    {
+        const string RemoteExecutionName = "MyRemoteServer";
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Creating RemoteExecutionServer...");
+
+            var remoteServer = new RemoteExecutionServer(RemoteExecutionName);
+
+            var pipeClient = new NamedPipeClientStream(".", RemoteExecutionName, PipeDirection.InOut, PipeOptions.Asynchronous);
+            pipeClient.Connect();
+
+            Console.WriteLine("Connection established");
+
+            while(true)
+            {
+                Console.Write("Message to send: ");
+                var bytesToSend = Encoding.UTF8.GetBytes(Console.ReadLine());
+                pipeClient.WriteAsync(bytesToSend, 0, bytesToSend.Length).Wait();
+            }
+        }
+    }
+}
