0364a7d2ac46407eca3bced6246c8fcc790175e3
Mike Rousos <MikeRou@Microsoft.com>
2016-12-01 07:32:52 -0800

    Update test client to run as server or client (rather than both)

--- TestClient/Program.cs
+++ TestClient/Program.cs
@@ -12,9 +12,12 @@ namespace TestClient
         const string RemoteExecutionName = "MyRemoteServer";
         public static void Main(string[] args)
         {
-            Console.WriteLine("Creating RemoteExecutionServer...");
+            if (args.Length < 1 || args[0] == "server")
+            {
+                Console.WriteLine("Creating RemoteExecutionServer...");
 
-            var remoteServer = new RemoteExecutionServer(RemoteExecutionName);
+                var remoteServer = new RemoteExecutionServer(RemoteExecutionName);
+            }
 
             //var pipeClient = new NamedPipeClientStream(".", RemoteExecutionName, PipeDirection.InOut, PipeOptions.Asynchronous);
             //pipeClient.Connect();
@@ -28,19 +31,25 @@ namespace TestClient
             //    pipeClient.WriteAsync(bytesToSend, 0, bytesToSend.Length).Wait();
             //}
 
-            Console.WriteLine("Creating proxy objects...");
+            if (args.Length < 1 || args[0] == "client")
+            {
+                Console.WriteLine("Creating proxy objects...");
 
-            var proxy3 = RemoteExecutionClient.CreateRemoteInstance<List<int>>(RemoteExecutionName);
-            Console.WriteLine($"Created remote object with ID: {proxy3.ID}");
+                var proxy3 = RemoteExecutionClient.CreateRemoteInstance<List<int>>(RemoteExecutionName);
+                Console.WriteLine($"Created remote object with ID: {proxy3.ID}");
 
-            var proxy4 = RemoteExecutionClient.CreateRemoteInstance<RemoteProxy>(RemoteExecutionName, new List<string>(), proxy3.ID);
-            Console.WriteLine($"Created remote object with ID: {proxy4.ID}");
+                var proxy4 = RemoteExecutionClient.CreateRemoteInstance<RemoteProxy>(RemoteExecutionName, new List<string>(), proxy3.ID);
+                Console.WriteLine($"Created remote object with ID: {proxy4.ID}");
 
-            var proxy1 = RemoteExecutionClient.CreateRemoteInstance<string>(RemoteExecutionName);
-            Console.WriteLine($"Created remote object with ID: {proxy1.ID}");
+                var proxy1 = RemoteExecutionClient.CreateRemoteInstance<string>(RemoteExecutionName);
+                Console.WriteLine($"Created remote object with ID: {proxy1.ID}");
 
-            var proxy2 = RemoteExecutionClient.CreateRemoteInstance<Program>(RemoteExecutionName);
-            Console.WriteLine($"Created remote object with ID: {proxy2.ID}");
+                var proxy2 = RemoteExecutionClient.CreateRemoteInstance<Program>(RemoteExecutionName);
+                Console.WriteLine($"Created remote object with ID: {proxy2.ID}");
+            }
+
+            Console.WriteLine("Press enter to quit");
+            Console.ReadLine();
         }
     }
 }
