8cda190d8ff9e2e97cdb007dddfc9e3d4007f1d1
Mike Rousos <MikeRou@Microsoft.com>
2016-12-08 13:42:10 -0800

    Separate the test server and client into separate projects

--- TestClient/Program.cs
+++ TestClient/Program.cs
@@ -3,54 +3,36 @@ using System.Collections.Generic;
 
 using RemoteExecution;
 
-
+// Simple test client to demonstrate remoting functionality with some test input
 namespace TestClient
 {
     public class Program
     {
         const string RemoteExecutionName = "MyRemoteServer";
 
-        static RemoteExecutionServer RemoteServer;
         public static void Main(string[] args)
         {
-            if (args.Length < 1 || args[0] == "server")
-            {
-                Console.WriteLine("Creating RemoteExecutionServer...");
+            Console.WriteLine("Creating proxy objects...");
 
-                RemoteServer = new RemoteExecutionServer(RemoteExecutionName);
+            var proxy0 = RemoteExecutionClient.CreateRemoteInstance<int>(RemoteExecutionName);
+            Console.WriteLine($"Created remote object with ID: {proxy0.ID}");
 
-                // Enable verbose logging
-                RemoteServer.Logger.LogLevel = MessagePriority.Verbose;
-            }
-            
-            if (args.Length < 1 || args[0] == "client")
-            {
-                Console.WriteLine("Creating proxy objects...");
+            var proxy1 = RemoteExecutionClient.CreateRemoteInstance<string>(RemoteExecutionName, "Hello, world!".ToCharArray());
+            Console.WriteLine($"Created remote object with ID: {proxy1.ID}");
 
-                var proxy0 = RemoteExecutionClient.CreateRemoteInstance<int>(RemoteExecutionName);
-                Console.WriteLine($"Created remote object with ID: {proxy0.ID}");
+            var proxy2 = RemoteExecutionClient.CreateRemoteInstance<List<double>>(RemoteExecutionName);
+            Console.WriteLine($"Created remote object with ID: {proxy2.ID}");
 
-                var proxy1 = RemoteExecutionClient.CreateRemoteInstance<string>(RemoteExecutionName, "Hello, world!".ToCharArray());
-                Console.WriteLine($"Created remote object with ID: {proxy1.ID}");
-                
-                var proxy2 = RemoteExecutionClient.CreateRemoteInstance<List<double>>(RemoteExecutionName);
-                Console.WriteLine($"Created remote object with ID: {proxy2.ID}");
+            var proxy3 = RemoteExecutionClient.CreateRemoteInstance<List<int>>(RemoteExecutionName, 5);
+            Console.WriteLine($"Created remote object with ID: {proxy3.ID}");
+            proxy3.InvokeAsync("Add", 3).Wait();
+            proxy3.InvokeAsync("Add", 5).Wait();
+            var index = proxy3.InvokeAsync<int>("IndexOf", 5).Result;
+            var count = proxy3.GetPropertyAsync<int>("Count").Result;
+            Console.WriteLine($"Added items to remote list (count: {count}) and found index of '5' is: {index}");
 
-                var proxy3 = RemoteExecutionClient.CreateRemoteInstance<List<int>>(RemoteExecutionName, 5);
-                Console.WriteLine($"Created remote object with ID: {proxy3.ID}");
-                proxy3.InvokeAsync("Add", 3).Wait();
-                proxy3.InvokeAsync("Add", 5).Wait();
-                var index = proxy3.InvokeAsync<int>("IndexOf", 5).Result;
-                var count = proxy3.GetPropertyAsync<int>("Count").Result;
-                Console.WriteLine($"Added items to remote list (count: {count}) and found index of '5' is: {index}");
-
-                var proxy4 = RemoteExecutionClient.CreateRemoteInstance<RemoteProxy>(RemoteExecutionName, typeof(Program), RemoteExecutionName, null);
-                Console.WriteLine($"Created remote object with ID: {proxy4.ID}");
-
-            }
-
-            Console.WriteLine("Press enter to quit");
-            Console.ReadLine();
+            var proxy4 = RemoteExecutionClient.CreateRemoteInstance<RemoteProxy>(RemoteExecutionName, typeof(Program), RemoteExecutionName, null);
+            Console.WriteLine($"Created remote object with ID: {proxy4.ID}");
         }
     }
 }
---
+++ TestServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using RemoteExecution;
+
+namespace TestServer
+{
+    public class Program
+    {
+        const string RemoteExecutionName = "MyRemoteServer";
+        static RemoteExecutionServer RemoteServer;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Creating RemoteExecutionServer...");
+
+            RemoteServer = new RemoteExecutionServer(RemoteExecutionName);
+
+            // Enable verbose logging
+            RemoteServer.Logger.LogLevel = MessagePriority.Verbose;
+
+            // Report server creation and wait
+            Console.WriteLine($"Remote server created with name {RemoteServer.RemoteName}");
+            Console.WriteLine("Press enter to quit");
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
